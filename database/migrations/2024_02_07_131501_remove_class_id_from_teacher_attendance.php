a4a886b9c3081d26cd74623fad8f006468b8108f
khemsingh27 <khem.singh@poplify.com>
2024-02-17 00:19:30 +0530

    update

---
+++ database/migrations/2024_02_07_131501_remove_class_id_from_teacher_attendance.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class RemoveClassIdFromTeacherAttendance extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('teacher_attendance', function (Blueprint $table) {
+            $table->dropColumn('class_id');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teacher_attendance', function (Blueprint $table) {
+            $table->unsignedBigInteger('class_id');
+        });
+    }
+}
